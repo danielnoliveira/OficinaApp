282625f03a428a4e9354efb798093db10a999bb9
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-05-24 12:51:39 -0300

    Troca dos componentes modularizados, mudanças na estilização.

--- App.js
+++ App.js
@@ -18,7 +18,8 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import axios from 'axios';
-import ListOrcamento from './src/ListOrcamento';
+import ListOrcamento from './src/components/ListOrcamento';
+import ModalItem from './src/components/ModalItem';
 
 const {width} = Dimensions.get('window');
 
@@ -41,37 +42,11 @@ const App = () => {
         <ActivityIndicator size="large" color="gray" />
       ) : (
         <View style={styles.contentList}>
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-              Alert.alert('Modal has been closed.');
-              setModalVisible(!modalVisible);
-            }}>
-            <View
-              style={[styles.centeredView, {backgroundColor: 'transparent'}]}>
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>
-                  Vendedor: {dataSelected?.seller}
-                </Text>
-                <Text style={styles.modalText}>
-                  Cliente: {dataSelected?.customer}
-                </Text>
-                <Text style={styles.modalText}>
-                  Valor:{dataSelected?.value}
-                </Text>
-                <Text style={styles.modalText}>
-                  Descrição: {dataSelected?.description}
-                </Text>
-                <TouchableOpacity
-                  style={[styles.button, styles.buttonClose]}
-                  onPress={() => setModalVisible(!modalVisible)}>
-                  <Text style={styles.textStyle}>Fechar</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </Modal>
+          <ModalItem
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+            dataSelected={dataSelected}
+          />
           <Text style={styles.headerTitle}>Lista de Orçamentos</Text>
           <ListOrcamento
             data={orcamento}
@@ -97,39 +72,9 @@ const styles = StyleSheet.create({
     width: width * 1,
     height: '100%',
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
     textAlign: 'center',
   },
   headerTitle: {
